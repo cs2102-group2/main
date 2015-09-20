0888d8a9b0653c751f030c416df60ef1015e6db2
bobby-lin <bobby.linjh@gmail.com>
2015-09-21 01:08:05 +0800

    User's Name is now shown at Homepage

--- carpool/index.php
+++ carpool/index.php
@@ -1,46 +1,52 @@
+<?php
+session_start();
+include 'sqlconn.php';
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <title>Homepage</title>
-  <link rel="stylesheet" href="./foundation/css/foundation.css" />
-  <link rel="stylesheet" href="./css/customise.css" />
+    <meta charset="UTF-8">
+    <title>Homepage</title>
+    <link rel="stylesheet" href="./foundation/css/foundation.css" />
+    <link rel="stylesheet" href="./css/customise.css" />
 </head>
 <body>
 <nav class="top-bar" data-topbar="" role="navigation">
-  <ul class="title-area">
-    <li class="name"><h1><a href="#">Bao Bao</a></h1></li>
-  </ul>
-  <section class="top-bar-section">
-  <ul class="right">
-    <li class="has-form show-for-large-up"><a href="#" class="button">$</a></li>
-    <li class="divider"></li>
-    <li class="has-form show-for-large-up"><a href="#" class="button">FIND RIDE</a></li>
-    <li class="divider"></li>
-    <li class="has-form show-for-large-up"><a href="#" class="button">OFFER RIDE</a></li>
-    <li class="divider"></li>
-    <li class="has-form show-for-large-up"><a href="#" class="button">LOGIN</a></li>
-  </ul>
-  </section>
+    <ul class="title-area">
+        <li class="name"><h1><a href="#"><?php echo $_SESSION["NAME"] ?></a></h1></li>
+    </ul>
+    <section class="top-bar-section">
+        <ul class="right">
+            <li class="has-form show-for-large-up"><a href="#" class="button">$</a></li>
+            <li class="divider"></li>
+            <li class="has-form show-for-large-up"><a href="#" class="button">FIND RIDE</a></li>
+            <li class="divider"></li>
+            <li class="has-form show-for-large-up"><a href="#" class="button">OFFER RIDE</a></li>
+            <li class="divider"></li>
+            <li class="has-form show-for-large-up"><a href="#" class="button">LOGIN</a></li>
+        </ul>
+    </section>
 </nav>
 <!-- End include -->
 
-  <div class="large-12 center-vertically columns">
+<div class="large-12 center-vertically columns">
     <form>
-      <div class="row">
-        <div class="large-4 columns">
-          <input type="text" name="departureSearch" placeholder="Departure" />
+        <div class="row">
+            <div class="large-4 columns">
+                <input type="text" name="departureSearch" placeholder="Departure" />
+            </div>
+            <div class="large-4 columns">
+                <input type="text" name="destinationSearch" placeholder="Destination" />
+            </div>
+            <div class="large-2 columns">
+                <input type="text" name="dateSearch" placeholder="Date" />
+            </div>
+            <div class="large-2 columns">
+                <a href="#" class="tiny button">SEARCH</a>
+            </div>
         </div>
-        <div class="large-4 columns">
-          <input type="text" name="destinationSearch" placeholder="Destination" />
-        </div>
-        <div class="large-2 columns">
-          <input type="text" name="dateSearch" placeholder="Date" />
-        </div>
-        <div class="large-2 columns">
-          <a href="#" class="tiny button">SEARCH</a>
-        </div>
-    </div>
     </form>
 </div>
 </body>
