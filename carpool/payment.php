4dfac07ca4bd886fc7ab81f24668c3d41705abfd
bobby-lin <bobby.linjh@gmail.com>
2015-09-21 13:19:08 +0800

    Added code to retrieve top-up amount from user

--- carpool/payment.php
+++ carpool/payment.php
@@ -1,7 +1,25 @@
 <?php
 
 include 'loadsession.php';
-include 'sqlconn.php';
+//include 'sqlconn.php';
+
+if(isset($_POST['makePayment'])) {
+    if(isset($_POST['topUpAmount'])) {
+        $topUpAmount = $_POST['topUpAmount'];
+
+        //================================================================
+        //Create a pop-out message to double-check amount:
+        //$msg = "Amount to top-up is ".$topUpAmount;
+        //echo "<script type='text/javascript'>alert('$msg');</script>";
+        //================================================================
+
+        // To Do: Add SQL query to update col 'ACCBALANCE' in table 'PROFILE'
+
+
+        // Simulate a Credit Card Deduction
+
+    }
+}
 
 ?>
 <!DOCTYPE html>
@@ -38,26 +56,31 @@ include 'sqlconn.php';
 </nav>
 <!-- End include -->
 <div class="large-12 center-vertically columns">
-    <div class="row">
-        <div class="large-6 large-offset-3 columns text-center white-translucent">
-            <h2>Your Account Balance:</h2>
-            <p id="userCurrency">$11.00</p>
-            <label id="topUpAmount">
-                <select class="text-center">
-                    <option class="placeholder" selected="selected" value= "" disabled="disabled">Select amount of credits to add</option>
-                    <option value="5">+ SGD 5.00</option>
-                    <option value="10">+ SGD 10.00</option>
-                    <option value="15">+ SGD 15.00</option>
-                    <option value="20">+ SGD 20.00</option>
-                </select>
-            </label>
+    <form method="post" >
+        <div class="row">
+            <div class="large-6 large-offset-3 columns text-center white-translucent">
+                <h2>Your Account Balance:</h2>
+                <p id="userCurrency">
+                    <?php getProfileAccountBalance() ?>
+                </p>
+                <label id="topUpAmount" action="payment.php">
+                    <select name = "topUpAmount" class="text-center">
+                        <option class="placeholder" selected="selected" value= "" disabled="disabled">Select amount of credits to add</option>
+                        <option value="5">+ SGD 5.00</option>
+                        <option value="10">+ SGD 10.00</option>
+                        <option value="15">+ SGD 15.00</option>
+                        <option value="20">+ SGD 20.00</option>
+                    </select>
+                </label>
+
+            </div>
         </div>
-    </div>
-    <div class="row">
-        <div class="large-6 large-offset-3 columns white-translucent">
-            <a href="#" class="large-12 small button">PROCEED TO PAYMENT</a>
+        <div class="row">
+            <div class="large-6 large-offset-3 columns white-translucent">
+                <input type="submit" name="makePayment" class="large-12 small button" value="MAKE PAYMENT" />
+            </div>
         </div>
-    </div>
+    </form>
 </div>
 </body>
 </html>
