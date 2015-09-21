4d4c7c7a7416c49cc42d1c8b1cfa5db67953355b
bobby-lin <bobby.linjh@gmail.com>
2015-09-21 10:35:09 +0800

    index.php can now pass search data to search.php

--- carpool/index.php
+++ carpool/index.php
@@ -1,6 +1,11 @@
 <?php
-session_start();
+
+include 'loadsession.php';
 include 'sqlconn.php';
+
+if(isUserLoggedIn() == false) {
+    redirectToLoginPage();
+}
 
 ?>
 
@@ -29,22 +34,21 @@ include 'sqlconn.php';
         </ul>
     </section>
 </nav>
-<!-- End include -->
 
 <div class="large-12 center-vertically columns">
-    <form>
+    <form id="searchBox" method="post" action="search.php">
         <div class="row">
             <div class="large-4 columns">
-                <input type="text" name="departureSearch" placeholder="Departure" />
+                <input type="text" id ="departureSearch" name="departureSearch" placeholder="Departure"/>
             </div>
             <div class="large-4 columns">
-                <input type="text" name="destinationSearch" placeholder="Destination" />
+                <input type="text" id ="destinationSearch" name="destinationSearch" placeholder="Destination"/>
             </div>
             <div class="large-2 columns">
-                <input type="text" name="dateSearch" placeholder="Date" />
+                <input type="text" id ="dateSearch" name="dateSearch" placeholder="Date"/>
             </div>
             <div class="large-2 columns">
-                <a href="#" class="tiny button">SEARCH</a>
+                <input type="submit" id="search" name="search" class="tiny button" value="SEARCH" />
             </div>
         </div>
     </form>
