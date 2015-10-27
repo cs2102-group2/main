40c5104668689f87961853fea312c927f14aa4b5
munkeat <a0129561@u.nus.edu>
2015-10-28 02:16:35 +0800

    Modification to DDL
    1. Rectify error in sample data (SAMPLE_TRIPS.sql)
    2. Added sequence to automatically generate receipt for TABLE Bookings's invoice receipt (CARPOOL_TABLE.sql)

--- carpool/sqlscripts/CARPOOL_DELETE.sql
+++ carpool/sqlscripts/CARPOOL_DELETE.sql
@@ -10,10 +10,12 @@ DROP TRIGGER trigger_tripdate_check;
 DROP TRIGGER trigger_profile;
 DROP TRIGGER trigger_bookings;
 DROP TRIGGER trigger_trips;
+DROP TRIGGER trigger_booking_invoice;
 
 DROP SEQUENCE seq_profile;
 DROP SEQUENCE seq_bookings;
 DROP SEQUENCE seq_trips;
+DROP SEQUENCE seq_invoice;
 
 DROP TABLE Bookings;
 DROP TABLE Trips;
